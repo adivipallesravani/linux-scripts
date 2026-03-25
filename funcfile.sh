4de483564bc39db1926a09f10ef1e5430ecb5a2e
Sravani <adivipallesravani99@gmail.com>
2026-03-25 21:28:39 +0530

    modified funcfile

--- funcfile.sh
+++ funcfile.sh
@@ -2,6 +2,12 @@
 
 function file_count() {
 local FILE="$1"
+
+if  [ ! -d "$FILE" ]; then
+ echo " $FILE isnt dir"
+return
+fi
+
 local COUNT=$(ls -1 "$FILE"| wc -l)
 echo "$FILE:"
 echo "$COUNT"
@@ -9,3 +15,4 @@ echo "$COUNT"
 file_count /etc
 file_count /var
 file_count /usr/bin
+file_count emo
