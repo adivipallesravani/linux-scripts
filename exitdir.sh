99cf2f72f282b454164b914730b092de3f6fa4a3
Sravani <adivipallesravani99@gmail.com>
2026-03-25 21:16:53 +0530

    modfied exitdir

--- exitdir.sh
+++ exitdir.sh
@@ -2,22 +2,18 @@
 
 FILE="$1"
 
-if [ -e $FILE ] 
-then
-	echo "file exists"
-	if [ -f $FILE ]
-	then
-		echo "it is a regular file"
-		exit 0
-	elif [ -d $FILE ]
-	then 
-		echo "it is a directory"
-		exit 1
-	else
-		echo "some other file"
-		exit 2
-	fi
-else
-	echo "file doesnt exist"
-        exit 3
+[ -z "$FILE" ] && { echo "Usage : $0 <file>"; exit 4; }
+[ ! -e "$FILE" ] && { echo "file doesnt exists"; exit 3;}
+
+echo "file exists"
+
+if [ -f "$FILE" ]; then 
+   echo " it is a normal file"
+   exit 0
+elif [ -d "$FILE" ]; then
+   echo "$FILE is a dir"
+   exit 1
+else 
+   echo "$FILE is other type"
+   exit 2 
 fi
